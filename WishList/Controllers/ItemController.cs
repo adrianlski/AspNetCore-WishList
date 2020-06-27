294409e2dc1386ac3df953efd9baecf04cde2656
Adrian <adrianlski@gmail.com>
2020-06-27 16:06:33 +0200

    Progress

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -35,10 +35,10 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int Id)
+        public IActionResult Delete(int id)
         {
-            var itemToDelete = _context.Items.Single(x => x.Id == Id);
-            _context.Items.Remove(itemToDelete);
+            var item = _context.Items.FirstOrDefault(x => x.Id == id);
+            _context.Items.Remove(item);
             _context.SaveChanges();
 
             return RedirectToAction("Index");
